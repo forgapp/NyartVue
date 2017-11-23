90ecd0f78b74b342e1eb4fc24082c6fb2b66aa08
Azurock <azurock@debian>
2017-11-24 00:42:04 +0900

    feat (Phone): adds phone display & edit component

---
+++ app/src/components/phones/display.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-if="!hasResults">No Phones</li>
+    <li class="phoneElement" v-for="(phone, index) in phones" :key="index">
+      <span class="icon leftIcon">
+        <i class="fa fa-phone" aria-hidden="true"></i>
+      </span>
+      <a :href="`tel:${phone.Number}`">{{ phone.Number }}</a>
+      <small class="rigthIcon" v-if="phone.Type">({{ phone.Type }})</small>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'phoneDisplay',
+    props: ['phones'],
+    computed: {
+      hasResults: function () {
+        return this.phones && this.phones.length > 0;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .phoneElement {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.5em;
+  }
+
+  .phoneElement .leftIcon {
+    margin-right: 0.5em;
+  }
+
+  .phoneElement .rigthIcon {
+    margin-left: 0.5em;
+  }
+</style>
---
+++ app/src/components/phones/edit.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="field has-addons" v-for="(phone, index) in phones">
+      <p class="control">
+        <Selectbox
+          placeholder="Type"
+          type="PhoneType"
+          :value="phone.Type"
+          @input="changeType(index, $event)"
+        />
+      </p>
+      <p class="control is-expanded">
+        <input class="input" :value="phone.Number" @change="changeNumber(index, $event)"/>
+      </p>
+      <p class="control">
+        <button class="button is-danger" @click.prevent="deletePhone(index)">
+          <i class="fa fa-trash"></i>
+        </button>
+      </p>
+    </div>
+
+    <div class="field ">
+      <div class="control">
+        <button
+          @click="addNewPhone"
+          class="button is-text"
+        >
+          Add a phone
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PhonesEdit',
+    props: [ 'phones' ],
+    methods: {
+      changeType: function (index, value) {
+        const phone = Object.assign({}, this.phones[index], {
+          Type: value
+        });
+        const phones = [
+          ...this.phones.slice(0, index),
+          phone,
+          ...this.phones.slice(index + 1, this.phones.length)
+        ];
+
+        this.$emit('phoneChanged', phones);
+      },
+      changeNumber: function (index, event) {
+        const phone = Object.assign({}, this.phones[index], {
+          Number: event.target.value
+        });
+        const phones = [
+          ...this.phones.slice(0, index),
+          phone,
+          ...this.phones.slice(index + 1, this.phones.length)
+        ];
+
+        this.$emit('phoneChanged', phones);
+      },
+      deletePhone: function (index) {
+        const phones = [
+          ...this.phones.slice(0, index),
+          ...this.phones.slice(index + 1, this.phones.length)
+        ];
+
+        this.$emit('phoneChanged', phones);
+      },
+      addNewPhone: function () {
+        const phones = [ ...this.phones, { Type: '', Number: '' } ];
+        this.$emit('phoneChanged', phones);
+      }
+    }
+  };
+</script>
